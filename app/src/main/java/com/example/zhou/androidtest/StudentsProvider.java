17a345ba0162809ff39b5aeb5c1417f61849f8ca
bo-zhou <zhou@enish.jp>
2016-05-01 13:49:32 +0900

    commit some examples

---
+++ app/src/main/java/com/example/zhou/androidtest/StudentsProvider.java
@@ -0,0 +1,325 @@
+package com.example.zhou.androidtest;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import java.util.HashMap;
+
+/**
+ * Created by zhou on 16/04/27.
+ */
+public class StudentsProvider extends ContentProvider {
+    private static final String TAG = StudentsProvider.class.getSimpleName();
+    private final StudentsProvider self = this;
+    public static final String PROVIDER_NAME = "com.example.zhou.androidtest.provider";
+    public static final String URL = "content://" + PROVIDER_NAME + "/students";
+    public static final Uri CONTENT_URI = Uri.parse(URL);
+
+    public static final String _ID = "_id";
+    public static final String NAME = "name";
+    public static final String GRADE = "grade";
+
+    private static HashMap<String, String> STUDENTS_PROJECTION_MAP;
+
+    public static final int STUDENTS = 1;
+    public static final int STUDENT_ID = 2;
+
+
+    static UriMatcher uriMatcher = null;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "students", STUDENTS);
+        uriMatcher.addURI(PROVIDER_NAME, "students/#", STUDENT_ID);
+    }
+
+    private SQLiteDatabase db;
+    static final String DATABASE_NAME = "college";
+    static final String STUDENTS_TABLE_NAME = "students";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_DB_TABLE =
+            " CREATE TABLE " + STUDENTS_TABLE_NAME +
+                    " (_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " name TEXT NOT NULL, " +
+                    " grade TEXT NOT NULL);";
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper (Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        /**
+         * Called when the database is created for the first time. This is where the
+         * creation of tables and the initial population of the tables should happen.
+         *
+         * @param db The database.
+         */
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DB_TABLE);
+        }
+
+        /**
+         * Called when the database needs to be upgraded. The implementation
+         * should use this method to drop tables, add tables, or do anything else it
+         * needs to upgrade to the new schema version.
+         * <p/>
+         * <p>
+         * The SQLite ALTER TABLE documentation can be found
+         * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+         * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+         * you can use ALTER TABLE to rename the old table, then create the new table and then
+         * populate the new table with the contents of the old table.
+         * </p><p>
+         * This method executes within a transaction.  If an exception is thrown, all changes
+         * will automatically be rolled back.
+         * </p>
+         *
+         * @param db         The database.
+         * @param oldVersion The old database version.
+         * @param newVersion The new database version.
+         */
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + STUDENTS_TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
+    public boolean onCreate() {
+        Context context = getContext();
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+        db = dbHelper.getWritableDatabase();
+        return (db == null) ? false : true;
+    }
+
+    /**
+     * Implement this to handle query requests from clients.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     * <p/>
+     * Example client call:<p>
+     * <pre>// Request a specific record.
+     * Cursor managedCursor = managedQuery(
+     * ContentUris.withAppendedId(Contacts.People.CONTENT_URI, 2),
+     * projection,    // Which columns to return.
+     * null,          // WHERE clause.
+     * null,          // WHERE clause value substitution
+     * People.NAME + " ASC");   // Sort order.</pre>
+     * Example implementation:<p>
+     * <pre>// SQLiteQueryBuilder is a helper class that creates the
+     * // proper SQL syntax for us.
+     * SQLiteQueryBuilder qBuilder = new SQLiteQueryBuilder();
+     *
+     * // Set the table we're querying.
+     * qBuilder.setTables(DATABASE_TABLE_NAME);
+     *
+     * // If the query ends in a specific record number, we're
+     * // being asked for a specific record, so set the
+     * // WHERE clause in our query.
+     * if((URI_MATCHER.match(uri)) == SPECIFIC_MESSAGE){
+     * qBuilder.appendWhere("_id=" + uri.getPathLeafId());
+     * }
+     *
+     * // Make the query.
+     * Cursor c = qBuilder.query(mDb,
+     * projection,
+     * selection,
+     * selectionArgs,
+     * groupBy,
+     * having,
+     * sortOrder);
+     * c.setNotificationUri(getContext().getContentResolver(), uri);
+     * return c;</pre>
+     *
+     * @param uri           The URI to query. This will be the full URI sent by the client;
+     *                      if the client is requesting a specific record, the URI will end in a record number
+     *                      that the implementation should parse and add to a WHERE or HAVING clause, specifying
+     *                      that _id value.
+     * @param projection    The list of columns to put into the cursor. If
+     *                      {@code null} all columns are included.
+     * @param selection     A selection criteria to apply when filtering rows.
+     *                      If {@code null} then all rows are included.
+     * @param selectionArgs You may include ?s in selection, which will be replaced by
+     *                      the values from selectionArgs, in order that they appear in the selection.
+     *                      The values will be bound as Strings.
+     * @param sortOrder     How the rows in the cursor should be sorted.
+     *                      If {@code null} then the provider is free to define the sort order.
+     * @return a Cursor or {@code null}.
+     */
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(STUDENTS_TABLE_NAME);
+        switch (uriMatcher.match(uri)) {
+            case STUDENTS:
+                qb.setProjectionMap(STUDENTS_PROJECTION_MAP);
+                break;
+
+            case STUDENT_ID:
+                qb.appendWhere( _ID + "=" + uri.getPathSegments().get(1) );
+                break;
+
+            default :
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        if( sortOrder == null || sortOrder == "" ) {
+            sortOrder = NAME;
+        }
+
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null , sortOrder);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+
+    /**
+     * Implement this to handle requests for the MIME type of the data at the
+     * given URI.  The returned MIME type should start with
+     * <code>vnd.android.cursor.item</code> for a single record,
+     * or <code>vnd.android.cursor.dir/</code> for multiple items.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     * <p/>
+     * <p>Note that there are no permissions needed for an application to
+     * access this information; if your content provider requires read and/or
+     * write permissions, or is not exported, all applications can still call
+     * this method regardless of their access permissions.  This allows them
+     * to retrieve the MIME type for a URI when dispatching intents.
+     *
+     * @param uri the URI to query.
+     * @return a MIME type string, or {@code null} if there is no type.
+     */
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        switch( uriMatcher.match(uri) ) {
+            case STUDENTS:
+                return "vnd.android.cursor.dir/vnd.example.students";
+            case STUDENT_ID:
+                return "vnd.android.cursor.item/vnd.example.students";
+            default:
+                throw new IllegalArgumentException("Unsupport URI: " + uri);
+        }
+    }
+
+    /**
+     * Implement this to handle requests to insert a new row.
+     * As a courtesy, call {@link ContentResolver#notifyChange(Uri, ContentObserver) notifyChange()}
+     * after inserting.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     *
+     * @param uri    The content:// URI of the insertion request. This must not be {@code null}.
+     * @param values A set of column_name/value pairs to add to the database.
+     *               This must not be {@code null}.
+     * @return The URI for the newly inserted item.
+     */
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long rowID = db.insert( STUDENTS_TABLE_NAME, "", values);
+        if( rowID > 0 ) {
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+        throw new SQLException("Failed to add a record into "+ uri);
+    }
+
+    /**
+     * Implement this to handle requests to delete one or more rows.
+     * The implementation should apply the selection clause when performing
+     * deletion, allowing the operation to affect multiple rows in a directory.
+     * As a courtesy, call {@link ContentResolver#notifyChange(Uri, ContentObserver) notifyChange()}
+     * after deleting.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     * <p/>
+     * <p>The implementation is responsible for parsing out a row ID at the end
+     * of the URI, if a specific row is being deleted. That is, the client would
+     * pass in <code>content://contacts/people/22</code> and the implementation is
+     * responsible for parsing the record number (22) when creating a SQL statement.
+     *
+     * @param uri           The full URI to query, including a row ID (if a specific record is requested).
+     * @param selection     An optional restriction to apply to rows when deleting.
+     * @param selectionArgs
+     * @return The number of rows affected.
+     * @throws SQLException
+     */
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int count =0;
+        switch (uriMatcher.match(uri)) {
+            case STUDENTS:
+                count = db.delete(STUDENTS_TABLE_NAME, selection, selectionArgs);
+                break;
+            case STUDENT_ID:
+
+                String id = uri.getPathSegments().get(1);
+                count = db.delete( STUDENTS_TABLE_NAME, _ID + " = " + id +
+                        (!TextUtils.isEmpty(selection) ? " AND (" + selection + ")" : ""), selectionArgs
+                );
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI" + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    /**
+     * Implement this to handle requests to update one or more rows.
+     * The implementation should update all rows matching the selection
+     * to set the columns according to the provided values map.
+     * As a courtesy, call {@link ContentResolver#notifyChange(Uri, ContentObserver) notifyChange()}
+     * after updating.
+     * This method can be called from multiple threads, as described in
+     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html#Threads">Processes
+     * and Threads</a>.
+     *
+     * @param uri           The URI to query. This can potentially have a record ID if this
+     *                      is an update request for a specific record.
+     * @param values        A set of column_name/value pairs to update in the database.
+     *                      This must not be {@code null}.
+     * @param selection     An optional filter to match rows to update.
+     * @param selectionArgs
+     * @return the number of rows affected.
+     */
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case STUDENTS:
+                count = db.update(STUDENTS_TABLE_NAME, values, selection, selectionArgs);
+                break;
+            case STUDENT_ID:
+                count = db.update(STUDENTS_TABLE_NAME, values, _ID + " = " + uri.getPathSegments().get(1) +
+                        (!TextUtils.isEmpty(selection) ? " AND (" + selection + ")" : ""), selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unkown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+}
